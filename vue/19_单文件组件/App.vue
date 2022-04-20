3bed8a35648001eaac15899760c8b258e2f43d63
xiyangcanyi <37542652+xiyangcanyi@users.noreply.github.com>
2022-04-20 11:54:31 +0800

    Add files via upload

---
+++ vue/19_单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <school></school>
+      <student></student>
+    </div>
+</template>
+
+<script>
+//引入组件
+    import school from './School.vue'
+    import student from './Student1.vue'
+
+    export default {
+     
+      name:'App',
+      components:{
+        school,
+        student
+      }
+    }
+</script>
+
+<style>
+</style>
